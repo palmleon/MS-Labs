87b6dd033381f030e60baeea5c0f475c535deca4
palmleon <leonardo.palmucci@gmail.com>
2021-04-04 16:51:24 +0200

    (Ex2.1.3) Feat(to finish): Added Main Tree for the Carry Generator and Testbench for the Carry Generator

--- gr38_lab02/ex2.1.3/sim/carry_generator.vhd
+++ gr38_lab02/ex2.1.3/sim/carry_generator.vhd
@@ -37,7 +37,7 @@ architecture structural of carry_generator is
 			Pij, Gij:			OUT	std_logic);
 	end component Gen_Prop;
 
-	constant TreeHeight: integer := integer(log2(real(NBIT)));
+	constant tree_height: integer := integer(log2(real(NBIT)));
 	type SignalMatrix is array(NBIT downto 1) of std_logic_vector(NBIT downto 0);
 	signal p, g: SignalMatrix; -- propagate and generate signal matrix (breadboard-like structure)
 begin
@@ -51,11 +51,27 @@ begin
 	
 	G_1_0:	Gen_Gen port map (Pik=>p(1)(1), Gik=>g(1)(1), Gmj=>Cin, Gij=>g(1)(0)); -- exceptional block where Cin is inserted
 			
-	MainTree: 
-	for i in 1 to TreeHeight generate
-		MainTreeNested:
-		for j in 0 to NBIT/2**i-1 generate
-		-- TODO Main Tree	
+	main_tree: 
+		-- given i, j for a PG or a G block, by definition: i = (pos+1) * 2^h, j = 1 + pos * 2^h
+		-- h is the current height in the tree, where 0 is the height of the PG network (Blocks where i and j are the same)
+		-- pos is the horizontal position, counting from the right (right-most block in a row is block 0 for that row)
+		-- the main tree is the binary tree obtained when NBLOCKS = 1 (the only Cout is the one on the MSB) 
+	for h in 1 to tree_height generate 
+		main_tree_nested:
+		for pos in 0 to NBIT/2**h-1 generate
+			main_tree_G:
+			if pos = 0 generate
+				G_i_0: Gen_Gen port map    (Pik=>p((pos+1)*(2**h))(2**(h-1)+1), Gik=>g((pos+1)*(2**h))(2**(h-1)+1),
+										 	Gmj=>g(2**(h-1))(0), 
+										 	Gij=>g((pos+1)*(2**h))(0));
+			end generate;
+			main_tree_PG:
+			if pos > 0 generate
+				PG_i_j: Gen_Prop port map  (Pik=>p((pos+1)*(2**h))((2*pos+1)*2**(h-1)+1), 	Gik=>g((pos+1)*(2**h))((2*pos+1)*2**(h-1)+1), 
+											Pmj=>p((2*pos+1)*2**(h-1))(pos*2**h+1), 		Gmj=>g((2*pos+1)*2**(h-1))(pos*2**h+1), 
+											Pij=>p((pos+1)*(2**h))(pos*2**h+1), 			Gij=>g((pos+1)*(2**h))(pos*2**h+1));
+			end generate;
+	-- TODO Main Tree
 		end generate;		
 	end generate;	
 
@@ -69,11 +85,22 @@ end architecture;
 
 configuration CFG_carrygen_structural of carry_generator is
 	for structural
-		for all: Gen_Gen
+		for G_1_0: Gen_Gen
 			use configuration work.CFG_GeneralGenerate_BEHAVIORAL;
 		end for;
-		for all: Gen_Prop
-			use configuration work.CFG_GeneralPropagate_BEHAVIORAL;
+		for main_tree
+			for main_tree_nested
+				for main_tree_G
+					for all: Gen_Gen
+						use configuration work.CFG_GeneralGenerate_BEHAVIORAL;
+					end for;
+				end for;
+				for main_tree_PG
+					for all: Gen_Prop
+						use configuration work.CFG_GeneralPropagate_BEHAVIORAL;
+					end for;
+				end for;
+			end for;
 		end for;
     end for;
 end CFG_carrygen_structural;
--- gr38_lab02/ex2.1.3/sim/tb_carry_generator.vhd
+++ gr38_lab02/ex2.1.3/sim/tb_carry_generator.vhd
@@ -1,24 +1,64 @@
 library ieee; 
-use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.ALL;
+use STD.textio.all;
+use WORK.constants.all;
 
 entity TB_CARRY_GENERATOR is 
 end TB_CARRY_GENERATOR; 
 
 architecture TEST of TB_CARRY_GENERATOR is
 
-
-	component CARRY_GENERATOR is
+	component carry_generator is
 		generic (
-			NBIT :		integer := 32;
-			NBIT_PER_BLOCK: integer := 4);
+			NBIT :			integer := numBit;
+			NBLOCKS:	 	integer := (numBit / 4));
 		port (
 			A :		in	std_logic_vector(NBIT-1 downto 0);
 			B :		in	std_logic_vector(NBIT-1 downto 0);
 			Cin :	in	std_logic;
-			Co :	out	std_logic_vector((NBIT/NBIT_PER_BLOCK)-1 downto 0));
-	end component;
+			Co :	out	std_logic_vector(NBLOCKS-1 downto 0));
+	end component carry_generator;
 
+	signal A_s, B_s: std_logic_vector(3 downto 0);
+	signal Cin_s: std_logic;
+	signal Co_s: std_logic_vector(0 downto 0);
 begin
+	DUT: carry_generator generic map (4, 1)
+						 port map (A_s, B_s, Cin_s, Co_s);
 
+	Test: process
+		file 	 testfile: text;
+		variable testline: line;
+		variable A, B: bit_vector(3 downto 0);
+		variable Cin: bit; 
+		variable Cout_exp: bit_vector(0 downto 0);
+		variable char: character;
+		variable cnt: integer := 1;
+	begin
+		file_open(testfile, "tb_data.mem", read_mode);
+		while not endfile(testfile) loop
+			-- format of test line: A B Cin Cout(expected)
+			readline(testfile, testline);
+			read(testline, A); 		read(testline, char);
+			read(testline, B); 		read(testline, char);
+			read(testline, Cin);	read(testline, char);
+			read(testline, Cout_exp);
+			A_s <= to_stdlogicvector(A);
+			B_s <= to_stdlogicvector(B);
+			Cin_s <= to_stdulogic(Cin);
+			wait for 50 ns; -- long interval to be sure that computation has terminated
+			assert Co_s = to_stdlogicvector(Cout_exp) report "ERROR on Test " & integer'image(cnt);
+		end loop;
+		file_close(testfile);
+		wait;
+	end process;
 end TEST;
+
+configuration CFG_CarryGenTest of tb_carry_generator is
+	for TEST
+		for DUT: carry_generator
+			use	configuration work.CFG_carrygen_structural;
+		end for;
+	end for;
+end configuration;
